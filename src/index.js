4d55db2a5e92758303ecd2efe02e3625c6908aad
Andreizayaz <poznavauka@mail.ru>
2020-10-09 13:29:04 +0300

    fix: change sort algorithm in towel-sort.js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,15 @@ module.exports = function towelSort(matrix) {
 
     if (matrix == undefined || matrix.length === 0) return [];
 
-    let str = '';
+    let towelArray =[];
 
     for (let i = 0; i < matrix.length; i++) {
         if (i % 2 === 0) {
-            str += matrix[i].join();
+            towelArray= towelArray.concat(matrix[i]);
         } else {
-            str += ',' + matrix[i].reverse().join() + ',';
+            towelArray= towelArray.concat(matrix[i].reverse());
         }
     }
-
-    let towelArray = str.split(',');
-
-    if (towelArray[towelArray.length - 1] === '') {
-        towelArray.splice(towelArray.length - 1, 1);
-    }
-
+   
     return towelArray;
 }
